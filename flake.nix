e01a52e5febac0c281b8ef6aadbf177ef0b7a74a
Billy J Rhoades II <brhoades@rippling.com>
2025-02-09 09:02:42 -0800

    init repo with flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "test-go-rust-gprc";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }: let
+    pkgsFor = system: import nixpkgs {
+      inherit system;
+    }; in (flake-utils.lib.eachDefaultSystem (system: {
+      # envrc
+      devShells.default = with (pkgsFor system); mkShell {
+        buildInputs = [
+          rustup
+          go
+          protobuf
+        ];
+      };
+    }));
+}
